e8849de4fc8ce37981a6e0f547ad4a968cdbb7b9
mokwongame <ictplusud@gmail.com>
2023-09-13 15:03:45 +0900

    Voltmeter.h 생성

--- TestVoltmeter/TestVoltmeter.ino
+++ TestVoltmeter/TestVoltmeter.ino
@@ -8,7 +8,7 @@ void loop() {
   // 3.3V -> A0: 선 연결
   int nVolt = analogRead(A0);  // A0 포트에서 아날로그 전압값 읽음 -> 0~1023 숫자로 바꿈
   // 0V -> nVolt = 0; 5V -> nVolt = 1023 (2^10 = 1024)
-  double volt = 5. / 1023. * nVolt; // step -> 전압값
-  Serial.println("volt = " + String(volt)); // String(volt): double -> string
-  delay(1000); // 1000 ms 지연
+  double volt = 5. / 1023. * nVolt;          // step -> 전압값
+  Serial.println("volt = " + String(volt));  // String(volt): double -> string
+  delay(1000);                               // 1000 ms 지연
 }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "TestVoltmeter 완성"
+git commit -m "Voltmeter.h 생성"
 git push -u origin master
