41df2d79a17b93de844bd7f10b8e4adf8d1e4c5c
mokwongame <ictplusud@gmail.com>
2023-10-04 15:23:48 +0900

    ArduinoHub 스케치 추가

---
+++ ArduinoHub/ArduinoHub.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "StringTok.h"
+#include "Voltmeter.h"
+#include "LightSensor.h"
+#include "MyServo.h"
+#include "DcMotor.h"
+#include "RgbLed.h"
+#include "Buzzer.h"
+
+class ArduinoHub
+{
+public:
+	ArduinoHub(void) {}
+	~ArduinoHub() {}
+
+private:
+};
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "TestStringTok 스케치에서 StringTok 확인"
+git commit -m "ArduinoHub 스케치 추가"
 git push -u origin master
